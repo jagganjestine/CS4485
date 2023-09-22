c11efaeb19e3f8024cf52c41ee6ff52184cf7e8d
Taha <taha.ahmed1786@gmail.com>
2023-09-21 21:25:33 -0500

    Co-authored-by: Jaggan Jestine <jaj180010@users.noreply.github.com> Co-authored-by: Rikhab Yusuf <ryusuf1@users.noreply.github.com>

--- online-tutoring/src/components/Login.js
+++ online-tutoring/src/components/Login.js
@@ -1,22 +1,53 @@
-// src/components/Login.js
 import React, { useState } from "react";
 import firebase from "../firebase";
-import { getAuth, signInWithEmailAndPassword  } from "firebase/auth";
-
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import emailjs from '@emailjs/browser';
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
- const auth = getAuth()
-
-  const handleLogin = async () => {
+  const [randomCode, setRandomCode] = useState(null);
+  const [userCode, setUserCode] = useState("");
+  const auth = getAuth();
+ 
+  const sendVerificationCode = async () => {
     try {
+      // Check if credentials are correct by attempting to sign in
       await signInWithEmailAndPassword(auth, email, password);
-      console.log("Successful Login");
-      // User logged in successfully
+
+      // If signInWithEmailAndPassword doesn't throw an error, credentials are correct
+      const min = 100000;
+      const max = 999999;
+      const generatedCode = Math.floor(Math.random() * (max - min + 1)) + min;
+      setRandomCode(generatedCode);
+      
+      const formValues = {
+        toemail: email,
+        message: generatedCode,
+      };
+      
+      emailjs.send("service_obyxgvu", "template_zhy667a", formValues, "BujL3gtXVBlpKdIYE")
+        .then((result) => {
+          alert("Code has been sent. Check your email!")
+          console.log("Code has been sent");
+        }, (error) => {
+          console.log(error.text);
+        });
     } catch (error) {
-      // Handle login errors
-      console.error(error);
+      console.error("Invalid credentials:", error);
+      alert("Invalid credentials. Please try again.");  // Inform the user about the incorrect credentials
+    }
+  };
+
+  const verifyCode = async () => {
+    if (parseInt(userCode) === randomCode) {
+      console.log("Verification successful");
+      setRandomCode(null);
+      // Since the user is already logged in after credential check, no need to sign in again.
+      // <---- REDIRECT TO USER HOMEPAGE ------->
+    } else {
+      console.log("Verification failed");
+      alert("Verification failed. Please try again.");
     }
   };
 
@@ -33,7 +64,18 @@ function Login() {
         placeholder="Password"
         onChange={(e) => setPassword(e.target.value)}
       />
-      <button onClick={handleLogin}>Login</button>
+      <button onClick={sendVerificationCode}>Send Verification Code</button>
+      {randomCode && (
+        <div>
+          <h3>Enter verification code sent to your email</h3>
+          <input
+            type="number"
+            placeholder="Verification Code"
+            onChange={(e) => setUserCode(e.target.value)}
+          />
+          <button onClick={verifyCode}>Verify and Login</button>
+        </div>
+      )}
     </div>
   );
 }
--- online-tutoring/src/components/Registration.js
+++ online-tutoring/src/components/Registration.js
@@ -1,21 +1,34 @@
-// src/components/Registration.js
 import React, { useState } from "react";
 import firebase from "../firebase";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-
 
 function Registration() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const auth = getAuth();
 
+  const isPasswordStrong = (password) => {
+    const minLength = 8;
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasDigit = /[0-9]/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(password);
+    
+    return password.length >= minLength && hasUppercase && hasLowercase && hasDigit && hasSpecialChar;
+  }
+
   const handleRegistration = async () => {
+    if (!isPasswordStrong(password)) {
+      alert("Password is too weak. Please ensure your password has at least 8 characters, includes an uppercase letter, a lowercase letter, a digit, and a special character.");
+      return;
+    }
+
     try {
       await createUserWithEmailAndPassword(auth, email, password);
-      // User registered successfully
       console.log("Success");
+      // REDIRECT TO LOGIN PAGE
     } catch (error) {
-      // Handle registration errors
+      alert("Something went wrong, Please try again.")
       console.error(error);
     }
   };
@@ -33,6 +46,7 @@ function Registration() {
         placeholder="Password"
         onChange={(e) => setPassword(e.target.value)}
       />
+      <p> Please ensure your password has at least 8 characters, includes an uppercase letter, a lowercase letter, a digit, and a special character.</p>
       <button onClick={handleRegistration}>Register</button>
     </div>
   );
