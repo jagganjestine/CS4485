f3b2d5a4d4fa7bfd8987957b51a8dc0260db6cc7
Sharmin Gaziani <sxg190141@utdallas.edu>
2023-10-10 22:07:15 -0500

    Navbar updates + css changes

--- online-tutoring/src/components/RegistrationSelection.js
+++ online-tutoring/src/components/RegistrationSelection.js
@@ -1,20 +1,32 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import './RegistrationSelection.css'; 
+import RegistrationTutors from './RegistrationTutors'
+import Registration from './Registration'
+import Logout from './Logout'
+import HomePage from './Homepage'
+import Login from './Login'
+import BottomNav from './BottomNav'
 
 function RegistrationSelection() {
     return (
+        <div>
         <div className="registration-container">
           <div className="registration-form">
             <h1 className="rsHeader">Are you a</h1>
             <div className="buttons-container">
-              <Link to="/registration" className="registration-button">Student</Link>
+              <Link to="/registration" style={{ textDecoration: 'none' }}className="registration-button">Student</Link>
               <span className="or-text">or</span>
-              <Link to="/registrationtutors" className="registration-button">Tutor</Link>
-              <Link to="/"> <button className="sign-up-button">Login</button> </Link>
+              <Link to="/registrationtutors" style={{ textDecoration: 'none' }}className="registration-button">Tutor</Link>
             </div>
           </div>
         </div>
+
+    <BottomNav />
+    
+    </div>
+
       );
     }
 
