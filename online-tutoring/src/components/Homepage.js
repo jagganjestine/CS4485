b8548a600398a47c16b5cb564750677788abe1e6
jagganjestine <78127002+jagganjestine@users.noreply.github.com>
2023-11-28 11:59:00 -0600

    fixes upcoming appointments date stud hp

--- online-tutoring/src/components/Homepage.js
+++ online-tutoring/src/components/Homepage.js
@@ -53,15 +53,18 @@ function HomePage() {
       console.error(error);
     }
   };
+  
   // used to reformat date for upcoming appointments (student hp)
 function formatDate(inputDate) {
-  const date = new Date(inputDate);
+  // Append 'T00:00:00' to make sure the date is parsed in local time
+  const date = new Date(`${inputDate}T00:00:00`);
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
   
   return `${month}/${day}/${year}`;
 }
+
 
 // used to reformat time for upcoming appointments (student hp)
 function formatTime(inputTime) {
